9c8a8d91c4c9809dbb1a86d422a7689d8bc870f4
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-04 19:49:59 +0530

    create config

---
+++ backend/src/main/java/lk/ijse/gdse/backend/config/ApplicationConfig.java
@@ -0,0 +1,37 @@
+package lk.ijse.gdse.backend.config;
+
+import lk.ijse.gdse.backend.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.List;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUserName(username)
+                .map(user -> new User(
+                        user.getUserName(),
+                        user.getUserPassword(),
+                        List.of(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+                )).orElseThrow(
+                        () -> new RuntimeException("User not found")
+                );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}

89047704eae4f79835f7dd27d6cd68f50ddabde2
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-05 20:56:25 +0530

    update UserService

--- backend/src/main/java/lk/ijse/gdse/backend/service/UserService.java
+++ backend/src/main/java/lk/ijse/gdse/backend/service/UserService.java
@@ -10,5 +10,8 @@ public interface UserService {
     List<UserDto> getAllUser();
     UserDto saveUser(UserDto userDto);
     UserDto updateUser(UserDto userDto);
-    void deleteCustomer(Long id);
+    void deleteUser(Long id);
+    List<UserDto> getUserByPage(int page, int size);
+    int getTotalPages(int size);
+    List<UserDto> searchUsers(String keyword);
 }

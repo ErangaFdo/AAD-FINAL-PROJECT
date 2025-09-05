245ba901f5f5078c04f6ee6afbb0926d8f178930
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-05 20:56:02 +0530

    update UserRepository

--- backend/src/main/java/lk/ijse/gdse/backend/repository/UserRepository.java
+++ backend/src/main/java/lk/ijse/gdse/backend/repository/UserRepository.java
@@ -2,11 +2,22 @@ package lk.ijse.gdse.backend.repository;
 
 import lk.ijse.gdse.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUserName(String username);
+
+    @Query(value = "SELECT * FROM users LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<User> findByUserPaginated(@Param("limit") int limit, @Param("offset") int offset);
+
+    @Query(value = "SELECT COUNT(*) FROM users", nativeQuery = true)
+    int getTotalUserCount();
+
+    List<User> findUserByUserNameContainingIgnoreCase(String keyword);
 }

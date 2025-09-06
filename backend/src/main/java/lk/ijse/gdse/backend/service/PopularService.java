6ac347b23b19b5a14bbf14f7a6e7d53f19ebe65e
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-06 07:58:40 +0530

    create PopularService

---
+++ backend/src/main/java/lk/ijse/gdse/backend/service/PopularService.java
@@ -0,0 +1,14 @@
+package lk.ijse.gdse.backend.service;
+
+import lk.ijse.gdse.backend.dto.PopularDto;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface PopularService {
+    void savePopular(PopularDto popularDto);
+    List<PopularDto> getAllPopularIte();
+    void updatePopular(PopularDto popularDto);
+    void deletePopular(Long id);
+}

6d56235b5eeea2230569735a7877768e5526ff8c
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-06 09:38:38 +0530

    create FeedBack entity

---
+++ backend/src/main/java/lk/ijse/gdse/backend/entity/FeedBack.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse.backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FeedBack {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long feedBackId;
+    private String fullName;
+    private String email;
+    private String services;
+    private String ratings;
+    private String message;
+
+    @ManyToOne
+    private User user;
+}

60e0c1be6c27f65049e92b0334a7c77a84836797
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-09 10:25:39 +0530

    create OrderEntity

---
+++ backend/src/main/java/lk/ijse/gdse/backend/entity/Order.java
@@ -0,0 +1,35 @@
+package lk.ijse.gdse.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderId;
+
+
+    private String name;
+    private double price;
+    private String email;
+    private String orderType;
+    private int orderQty;
+    private Date orderDatetime;
+    private String status;
+    private String notes;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}

1df379e6cb8e1b6c3f472c24372d179b4b345f7a
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-17 23:18:22 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/repository/OrderRepository.java
+++ backend/src/main/java/lk/ijse/gdse/backend/repository/OrderRepository.java
@@ -4,7 +4,10 @@ import lk.ijse.gdse.backend.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+    Optional<Object> findTopByUser_UserNameOrderByOrderDatetimeDesc(String username);
 }
+

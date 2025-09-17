8b9cef583b77a56c9c106c9c1618f03f051b810e
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-17 09:55:57 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/service/ProductService.java
+++ backend/src/main/java/lk/ijse/gdse/backend/service/ProductService.java
@@ -1,6 +1,7 @@
 package lk.ijse.gdse.backend.service;
 
 import lk.ijse.gdse.backend.dto.ProductDto;
+import lk.ijse.gdse.backend.dto.UserDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,4 +12,6 @@ public interface ProductService {
     List<ProductDto> getAllProduct();
     void updateProduct(ProductDto productDto);
     void deleteProduct(Long id);
+    List<ProductDto> getProductByPage(int page, int size);
+    int getTotalPages(int size);
 }

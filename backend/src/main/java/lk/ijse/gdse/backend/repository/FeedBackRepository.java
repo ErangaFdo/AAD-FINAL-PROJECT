cb86ff096168774736c810b6606db0f91d514995
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-17 09:52:37 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/repository/FeedBackRepository.java
+++ backend/src/main/java/lk/ijse/gdse/backend/repository/FeedBackRepository.java
@@ -2,8 +2,18 @@ package lk.ijse.gdse.backend.repository;
 
 import lk.ijse.gdse.backend.entity.FeedBack;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface FeedBackRepository extends JpaRepository<FeedBack, Long> {
+
+    @Query(value = "SELECT * FROM feed_back LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<FeedBack> findByFeedbackPaginated(@Param("size") int size, @Param("offset") int offset);
+
+    @Query(value = "SELECT COUNT(*) FROM feed_back", nativeQuery = true)
+    int getTotalFeedbackCount();
 }

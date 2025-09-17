b11b516d33a8ab0d1fccf10dc0dbd1c61eb0a98d
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-17 09:55:49 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/repository/ProductRepository.java
+++ backend/src/main/java/lk/ijse/gdse/backend/repository/ProductRepository.java
@@ -2,8 +2,18 @@ package lk.ijse.gdse.backend.repository;
 
 import lk.ijse.gdse.backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = "SELECT * FROM products LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<Product> findPaginatedProducts(@Param("limit") int size, @Param("offset") int offset);
+
+    @Query(value = "SELECT COUNT(*) FROM products", nativeQuery = true)
+    int getTotalProductCount();
 }

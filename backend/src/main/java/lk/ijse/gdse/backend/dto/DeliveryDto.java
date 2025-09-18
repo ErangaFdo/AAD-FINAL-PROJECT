cb9098f1d66702c9584f1e2b4fc048eee804bdcf
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-18 19:51:59 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/dto/DeliveryDto.java
+++ backend/src/main/java/lk/ijse/gdse/backend/dto/DeliveryDto.java
@@ -1,4 +1,24 @@
 package lk.ijse.gdse.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DeliveryDto {
+    private Long deliveryId;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String date;
+    private String time;
+    private Long orderId;
+
+    private Double latitude;
+    private Double longitude;
 }

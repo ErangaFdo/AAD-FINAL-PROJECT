cf490f2e056a689f51ddee7da656d065d0f68d73
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-18 13:48:25 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/repository/UserRepository.java
+++ backend/src/main/java/lk/ijse/gdse/backend/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package lk.ijse.gdse.backend.repository;
 
+import lk.ijse.gdse.backend.entity.Order;
 import lk.ijse.gdse.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;

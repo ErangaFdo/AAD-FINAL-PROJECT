c7ec1fc71d0efe5b32d582dc853ae46e2bb0243d
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-18 19:50:51 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/entity/Delivery.java
+++ backend/src/main/java/lk/ijse/gdse/backend/entity/Delivery.java
@@ -1,4 +1,35 @@
 package lk.ijse.gdse.backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "delivery")
 public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long deliveryId;
+    private String fullName;
+    private String phoneNumber;
+    private String email;
+    private String address;
+    private String date;
+    private String time;
+
+    private Double latitude;
+    private Double longitude;
+
+
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+    private Order order;
 }

16355e5d56f3f489d82354f93ba23f276e045d24
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-18 19:51:50 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/controller/DeliveryController.java
+++ backend/src/main/java/lk/ijse/gdse/backend/controller/DeliveryController.java
@@ -1,4 +1,39 @@
 package lk.ijse.gdse.backend.controller;
 
+
+import lk.ijse.gdse.backend.dto.ApiResponse;
+import lk.ijse.gdse.backend.dto.DeliveryDto;
+import lk.ijse.gdse.backend.service.DeliveryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/delivery")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:63342", allowCredentials = "true")
 public class DeliveryController {
+
+    private final DeliveryService deliveryService;
+
+    @PostMapping("save")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity<ApiResponse> save(@RequestBody DeliveryDto deliveryDto) {
+        DeliveryDto saveDelivery  = deliveryService.createDelivery(deliveryDto);
+        return ResponseEntity.ok(
+                new ApiResponse(201, "Delivery Saved Successfully", saveDelivery)
+        );
+    }
+
+//    @GetMapping("/all")
+//    @PreAuthorize("hasRole('ADMIN')")
+//    public ResponseEntity<ApiResponse> getAllDelivery() {
+//        List<DeliveryDto> deliveryDtos = deliveryService.getAllDelivery();
+//        return ResponseEntity.ok(
+//                new ApiResponse(200, "OK", deliveryDtos)
+//        );
+//    }
 }

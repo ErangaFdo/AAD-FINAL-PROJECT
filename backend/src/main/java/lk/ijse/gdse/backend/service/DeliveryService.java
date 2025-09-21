067114837b759b7ed5b1026d074e2de193c3190d
ErangaFernando404 <erangafdo1994@gmail.com>
2025-09-21 12:16:34 +0530

    update

--- backend/src/main/java/lk/ijse/gdse/backend/service/DeliveryService.java
+++ backend/src/main/java/lk/ijse/gdse/backend/service/DeliveryService.java
@@ -3,7 +3,12 @@ package lk.ijse.gdse.backend.service;
 import lk.ijse.gdse.backend.dto.DeliveryDto;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public interface DeliveryService {
     DeliveryDto createDelivery(DeliveryDto deliveryDto);
+    List<DeliveryDto> getAllDelivery();
+    List<DeliveryDto> getDeliveryByPage(int page, int size);
+    int getTotalPages(int size);
 }
